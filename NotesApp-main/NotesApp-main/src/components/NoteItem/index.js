e56afd19a8e134ddb967c9598a692219a1b7a2f0
prudhvi2467 <100276368+prudhvi2467@users.noreply.github.com>
2024-06-14 08:21:29 -0700

    Add files via upload

---
+++ NotesApp-main/NotesApp-main/src/components/NoteItem/index.js
@@ -0,0 +1,16 @@
+// Write your code here
+// Write your code here
+import {ItemContainer, ItemTitle, ItemNote} from './styledComponents'
+
+const NoteItem = props => {
+  const {note} = props
+
+  return (
+    <ItemContainer>
+      <ItemTitle>{note.title}</ItemTitle>
+      <ItemNote>{note.note}</ItemNote>
+    </ItemContainer>
+  )
+}
+
+export default NoteItem
